b47cccf36401020b2fd6e32a757854297f27270b
Robert <orazurobert@yahoo.com>
2021-07-28 16:28:39 +0100

    adding the widget component that can used to create new columns

---
+++ client/src/components/AddColumnWidget/AddColumnWidget.tsx
@@ -0,0 +1,13 @@
+import { FC } from 'react';
+import useStyles from './useStyles';
+
+const AddColumnWidget: FC = (): JSX.Element => {
+  const classes = useStyles();
+  return (
+    <div className={classes.widget}>
+       
+    </div>
+  );
+};
+
+export default AddColumnWidget;
---
+++ client/src/components/AddColumnWidget/useStyles.ts
@@ -0,0 +1,11 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(() => ({
+  widget: {
+    width: 50,
+    height: '90vh',
+    backgroundColor: '#e5e5e5',
+  },
+}));
+
+export default useStyles;
