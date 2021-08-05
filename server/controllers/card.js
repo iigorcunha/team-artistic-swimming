b45e2296acb4a5de65cea3b73b9df999ea52b4c5
Igor Cunha <igorcesarcunha@gmail.com>
2021-08-05 14:46:07 -0400

    fix: move Card to card controller

--- server/controllers/card.js
+++ server/controllers/card.js
@@ -6,17 +6,23 @@ const asyncHandler = require("express-async-handler");
 // @access Private
 exports.editCard = asyncHandler(async (req, res, next) => {
 
-  const { id } = req.params;
+  const { _id } = req.params;
 
   const {
+    name,
+    description,
     colorCode,
+    deadline,
     tags,
     attachments
   } = req.body;
 
   try {
-    await Card.findOneAndUpdate({_id: id}, {
+    await Card.findOneAndUpdate({_id}, {
+      name,
+      description,
       colorCode,
+      deadline,
       tags,
       attachments
     });
@@ -28,3 +34,18 @@ exports.editCard = asyncHandler(async (req, res, next) => {
   
   res.status(204).send();
 });
+
+
+exports.deleteCard = asyncHandler(async (req, res, next) => {
+  const { _id } = req.params;
+
+  
+    const card = await Card.findOneAndDelete({ _id })
+
+    if (!card) {
+      res.status(404);
+    throw new Error("Card can't be deleted with given id");
+    } else {
+      res.status(204).send()
+    }
+})
