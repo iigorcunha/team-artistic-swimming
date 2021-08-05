08f91cff1517d06152caf65d70d9d714d95da7ad
Igor Cunha <igorcesarcunha@gmail.com>
2021-08-05 14:48:07 -0400

    fix: move endpoint from app to index in routes

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,6 +3,7 @@ const authRouter = require("./auth");
 const userRouter = require("./user");
 const boardRouter = require("./board");
 const cardRouter = require("./card");
+const columnRouter = require("./column");
 
 const routes = Router();
 
@@ -10,5 +11,6 @@ routes.use("/auth", authRouter);
 routes.use("/users", userRouter);
 routes.use("/board", boardRouter);
 routes.use("/card", cardRouter);
+routes.use("/column", columnRouter);
 
 module.exports = routes;
