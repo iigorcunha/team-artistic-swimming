ec9404ccfaa75616821de3b7f536a52ba32eda57
Igor Cunha <igorcesarcunha@gmail.com>
2021-08-12 13:04:40 -0400

    fix: refactor type to match with backend and improve handling

--- client/src/interface/BoardApiData.ts
+++ client/src/interface/BoardApiData.ts
@@ -1,47 +1,44 @@
 import { Board } from './Board';
 
-export interface BoardResponse extends Board{
+export interface BoardWithoutNestedChildren {
+  _id: string;
+  columns: string[];
+  name: string;
+  lastViewed: boolean;
+}
+
+export interface BoardResponse extends Board {
   lastViewed: boolean;
   user: string;
-  __v: string;
 }
-export interface BoardApiDataSuccess {
-  message: string;
-  board: BoardResponse;
-}
-
 
 export interface BoardApiData {
-  error?: { message: string };
-  success?: BoardApiDataSuccess;
+  error?: string;
+  board?: Board;
 }
 
 export interface AllBoardResponse extends Omit<Board, 'columns'> {
   columns: string[];
   lastViewed: boolean;
   user: string;
-  __v: string;
 }
-
-export interface AllBoardApiDataSuccess {
-  message: string;
-  boardsList: AllBoardResponse[];
-}
-
 
 export interface AllBoardApiData {
-  error?: { message: string };
-  success?: AllBoardApiDataSuccess;
+  error?: string;
+  boards?: BoardWithoutNestedChildren[];
 }
-
 
 export interface CreateBoardSuccess {
   message: string;
   board: AllBoardResponse;
 }
 
-
 export interface CreateBoardApiData {
-  error?: { message: string };
+  error?: string;
   success?: CreateBoardSuccess;
+}
+
+export interface HandleBoardApiData {
+  error?: string;
+  board?: Board;
 }
