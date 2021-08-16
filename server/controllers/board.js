91e5df51ee660578d38c915f367f07240990d135
Robert <orazurobert@yahoo.com>
2021-08-16 18:24:18 +0100

    TeamBoard model creation and fixed line endings

--- server/controllers/board.js
+++ server/controllers/board.js
@@ -1,4 +1,4 @@
-const Board = require("../models/Board");
+const { Board } = require("../models/Board");
 const Card = require("../models/Card");
 const asyncHandler = require("express-async-handler");
 const BoardColumn = require("../models/BoardColumn");
