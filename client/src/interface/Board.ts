15012bbb60209860b402ba0a7ede498d15a0ef3d
Igor Cunha <igorcesarcunha@gmail.com>
2021-08-17 14:38:59 -0400

    fix: update card interface and responses.

--- client/src/interface/Board.ts
+++ client/src/interface/Board.ts
@@ -1,18 +1,48 @@
-export interface Card{
-    _id: string;
-    name: string;
-    color: ColorTags;
-    deadline?: string;
+export interface Comment {
+  _id: string;
+  username: string;
+  comment: string;
+  createdAt: Date;
+}
+
+export interface Item {
+  _id: string;
+  name: string;
+  done: boolean;
+}
+
+export interface Checklist {
+  _id: string;
+  name: string;
+  items: Item[];
+}
+
+export interface Attachment {
+  _id?: string;
+  name: string;
+  url: string;
+}
+
+export interface Card {
+  _id: string;
+  name: string;
+  colorCode: ColorTags;
+  deadline?: Date;
+  description?: string;
+  tags?: string[];
+  attachments?: Attachment[];
+  checklists?: Checklist[];
+  comments?: Comment[];
 }
 export interface Column {
-    _id: string;
-    cards: Card[];
-    name: string;
+  _id: string;
+  cards: Card[];
+  name: string;
 }
 
 export interface Board {
-    _id: string;
-    columns: Column[];
-    name: string;
+  _id: string;
+  columns: Column[];
+  name: string;
 }
-export type ColorTags = 'red' | 'blue' | 'yellow' | 'green' | ''
+export type ColorTags = 'red' | 'blue' | 'yellow' | 'green' | '';
