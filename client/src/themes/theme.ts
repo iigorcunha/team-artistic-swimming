6eb235ad465284dd9b934a89faa84f646090b027
Igor Cunha <igorcesarcunha@gmail.com>
2021-08-18 14:11:41 -0400

    edited shadows, to match with project colors.

--- client/src/themes/theme.ts
+++ client/src/themes/theme.ts
@@ -1,4 +1,5 @@
 import { createMuiTheme } from '@material-ui/core';
+import { Shadows } from '@material-ui/core/styles/shadows';
 
 declare module '@material-ui/core/styles/createPalette' {
   interface Palette {
@@ -36,4 +37,18 @@ export const theme = createMuiTheme({
   shape: {
     borderRadius: 5,
   },
+  shadows: [
+    'none',
+    '0px 2px 1px -1px rgba(155,169, 204, 0.2),0px 1px 1px 0px rgba(155,169, 204,0.14),0px 1px 3px 0px rgba(155,169, 204,0.12)',
+    '0px 3px 1px -2px rgba(155,169, 204,0.2),0px 2px 2px 0px rgba(155,169, 204,0.14),0px 1px 5px 0px rgba(155,169, 204,0.12)',
+    '0px 3px 3px -2px rgba(155,169, 204,0.2),0px 3px 4px 0px rgba(155,169, 204,0.14),0px 1px 8px 0px rgba(155,169, 204,0.12)',
+    '0px 2px 4px -1px rgba(155,169, 204,0.2),0px 4px 5px 0px rgba(155,169, 204,0.14),0px 1px 10px 0px rgba(155,169, 204,0.12)',
+    '0px 3px 5px -1px rgba(155,169, 204,0.2),0px 5px 8px 0px rgba(155,169, 204,0.14),0px 1px 14px 0px rgba(155,169, 204,0.12)',
+    '0px 3px 5px -1px rgba(155,169, 204,0.2),0px 6px 10px 0px rgba(155,169, 204,0.14),0px 1px 18px 0px rgba(155,169, 204,0.12)',
+    '0px 4px 5px -2px rgba(155,169, 204,0.2),0px 7px 10px 1px rgba(155,169, 204,0.14),0px 2px 16px 1px rgba(155,169, 204,0.12)',
+    '0px 5px 5px -3px rgba(155,169, 204,0.2),0px 8px 10px 1px rgba(155,169, 204,0.14),0px 3px 14px 2px rgba(155,169, 204,0.12)',
+    '0px 5px 6px -3px rgba(155,169, 204,0.2),0px 9px 12px 1px rgba(155,169, 204,0.14),0px 3px 16px 2px rgba(155,169, 204,0.12)',
+    '0px 6px 6px -3px rgba(155,169, 204,0.2),0px 10px 14px 1px rgba(155,169, 204,0.14),0px 4px 18px 3px rgba(155,169, 204,0.12)',
+    ...Array(15).fill('none'),
+  ] as Shadows,
 });
