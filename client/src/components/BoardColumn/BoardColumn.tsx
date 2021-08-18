1451adeb7baf5ca632b2068ce1a25da3993e655a
Igor Cunha <igorcesarcunha@gmail.com>
2021-08-18 14:07:57 -0400

    feat: implemented delete button and edit column name.

--- client/src/components/BoardColumn/BoardColumn.tsx
+++ client/src/components/BoardColumn/BoardColumn.tsx
@@ -1,10 +1,17 @@
-import Typography from '@material-ui/core/Typography';
+import { Box, IconButton, Input } from '@material-ui/core';
+import * as yup from 'yup';
+import { Formik } from 'formik';
 import { FC } from 'react';
 import { Draggable, Droppable } from 'react-beautiful-dnd';
+import { FiTrash2 } from 'react-icons/fi';
+import { usePopover } from '../../context/usePopoverContext';
 import { Card } from '../../interface/Board';
 import BoardCard from '../Card/Card';
 import NewCardForm from '../NewCardForm/NewCardForm';
+import { Popover } from '../Popover/Popover';
+import { BoardColumnDelete } from './BoardColumnDelete/BoardColumnDelete';
 import useStyles from './useStyles';
+import { useBoard } from '../../context/useBoardContext';
 
 interface ColumnProps {
   column: Card[];
@@ -14,14 +21,56 @@ interface ColumnProps {
 }
 
 const BoardColumn: FC<ColumnProps> = ({ column, droppableId, name, index }): JSX.Element => {
-  const classes = useStyles();
+  const { handleOpenPopover, handleClosePopover } = usePopover();
+  const { editableColumn, handleEditColumn, updateColumn } = useBoard();
+
+  const classes = useStyles({ editColumn: editableColumn === droppableId });
+
   return (
     <Draggable draggableId={droppableId} index={index}>
       {(provided) => (
         <div className={classes.columnContainer} {...provided.draggableProps} ref={provided.innerRef}>
-          <Typography className={classes.columnTitle} {...provided.dragHandleProps}>
-            {name}
-          </Typography>
+          <Box className={classes.columnTitleContainer}>
+            <Formik
+              enableReinitialize
+              initialValues={{ name: name }}
+              validationSchema={yup.object({
+                name: yup.string().required('Column name is required.'),
+              })}
+              onSubmit={(values, { setSubmitting }) => {
+                setSubmitting(false);
+
+                updateColumn({ _id: droppableId, name: values.name });
+
+                handleEditColumn('closed');
+              }}
+            >
+              {({ touched, errors, values, handleChange, submitForm }) => (
+                <Input
+                  {...provided.dragHandleProps}
+                  autoFocus={editableColumn === droppableId}
+                  id="name"
+                  name="name"
+                  value={values.name}
+                  disabled={!(editableColumn === droppableId)}
+                  disableUnderline
+                  onChange={handleChange}
+                  onClick={() => !(editableColumn === droppableId) && handleEditColumn(droppableId)}
+                  onKeyPress={(event) => event.key === 'Enter' && submitForm()}
+                  className={classes.columnTitle}
+                  error={touched.name && Boolean(errors.name)}
+                  onBlur={() => handleEditColumn('closed')}
+                />
+              )}
+            </Formik>
+
+            <IconButton onClick={handleOpenPopover} id={droppableId} className={classes.deleteButton}>
+              <FiTrash2 />
+            </IconButton>
+            <Popover onClose={handleClosePopover} name={droppableId}>
+              <BoardColumnDelete columnId={droppableId} />
+            </Popover>
+          </Box>
           <Droppable droppableId={droppableId} type="card">
             {(provided) => (
               <ul className={classes.column} {...provided.droppableProps} ref={provided.innerRef}>
