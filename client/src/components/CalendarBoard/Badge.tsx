9091c2ed15eb11a1fb1d706f5372aaa26452a71b
Igor Cunha <igorcesarcunha@gmail.com>
2021-08-20 11:48:14 -0400

    implemented chatDialog and months handle

--- client/src/components/CalendarBoard/Badge.tsx
+++ client/src/components/CalendarBoard/Badge.tsx
@@ -1,60 +1,38 @@
-import React, { useState, useRef } from 'react';
+import React from 'react';
 import { Event, EventProps, ToolbarProps } from 'react-big-calendar';
-import { Typography } from '@material-ui/core';
-import Popover from '@material-ui/core/Popover';
-
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
+import { IconButton } from '@material-ui/core';
 import useStyles from './useStyles';
+import { useCalendar } from '../../context/useCalendarContext';
+import { addMonths, subtractMonths } from '../../helpers/date/dateFormat';
 
 export const Badge: React.FC<EventProps<Event>> = ({ event }): JSX.Element => {
-  const classes = useStyles();
-
-  const [anchorEl, setAnchorEl] = useState<HTMLElement | null>(null);
-  const divRef = useRef<HTMLDivElement>(null);
-
-  const open = Boolean(anchorEl);
-  const id = open ? 'simple-popover' : undefined;
-
-  const handlePopoverOpen = (event: React.MouseEvent<HTMLElement, MouseEvent>) => {
-    setAnchorEl(event.currentTarget);
-  };
-
-  const handlePopoverClose = () => {
-    setAnchorEl(null);
-  };
-
   return (
-    <React.Fragment>
-      <div ref={divRef} onClick={handlePopoverOpen} className="rbc-event-content">
-        <div className="rbc-event-color" style={{ backgroundColor: event.resource }}></div>
-        <Typography className="rbc-event-label">{event.title}</Typography>
-      </div>
-      <Popover
-        id={id}
-        open={open}
-        anchorEl={anchorEl}
-        onClose={handlePopoverClose}
-        anchorOrigin={{
-          vertical: 'top',
-          horizontal: 'left',
-        }}
-        transformOrigin={{
-          vertical: 'top',
-          horizontal: 'left',
-        }}
-      >
-        <Typography className={classes.typography}>Task: {event.title} </Typography>
-        <Typography className={classes.typography}>Description: here you can add a description </Typography>
-      </Popover>
-    </React.Fragment>
+    <div className="rbc-event-content">
+      <div className="rbc-event-color" style={{ backgroundColor: event.resource }}></div>
+      <strong style={{ fontSize: '16px' }} className="rbc-event-label">
+        {event.title}
+      </strong>
+    </div>
   );
 };
 
 export const MonthTitle: React.FC<ToolbarProps> = ({ label }): JSX.Element => {
+  const classes = useStyles();
+  const { handleMonth, currentMonth } = useCalendar();
   return (
     <div className="rbc-toolbar">
-      <Typography variant="h4" className="rbc-toolbar-label">
-        {label}
-      </Typography>
+      <span className="rbc-btn-group">
+        <IconButton className={classes.toolbarButtons} onClick={() => handleMonth(subtractMonths(currentMonth, 1))}>
+          <FiChevronLeft />
+        </IconButton>
+      </span>
+      <span className="rbc-toolbar-label">{label}</span>
+      <span className="rbc-btn-group">
+        <IconButton className={classes.toolbarButtons} onClick={() => handleMonth(addMonths(currentMonth, 1))}>
+          <FiChevronRight />
+        </IconButton>
+      </span>
     </div>
   );
 };
