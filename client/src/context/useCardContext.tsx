c4f3502a8c39cfde68dbf9d1f328a745dfbcaf0e
Igor Cunha <igorcesarcunha@gmail.com>
2021-08-20 11:45:08 -0400

    implemented calendar logic to fetch cards.

--- client/src/context/useCardContext.tsx
+++ client/src/context/useCardContext.tsx
@@ -1,18 +1,21 @@
-import { useState, useContext, createContext } from 'react';
+import { useState, useContext, createContext, useCallback } from 'react';
 import cardDetails from '../helpers/APICalls/cardDetails';
 import deleteCard from '../helpers/APICalls/deleteCard';
 import deleteChecklist from '../helpers/APICalls/deleteChecklist';
 import deleteItem from '../helpers/APICalls/deleteItem';
+import getCards from '../helpers/APICalls/getCards';
 import listOneCard from '../helpers/APICalls/listOneCard';
 import patchChecklist from '../helpers/APICalls/patchChecklist';
 import patchItem from '../helpers/APICalls/patchItem';
 import postCard from '../helpers/APICalls/postCard';
-import { Card, CreateCard } from '../interface/Board';
+import { CalendarCards, Card, CreateCard } from '../interface/Board';
 import { UpdatedCard } from '../interface/CardApiData';
 import { CreateItem, UpdatedItem } from '../interface/ChecklistApiData';
+import { useBoard } from './useBoardContext';
 
 interface ICardContext {
   card: Card;
+  calendarCards: CalendarCards[];
   isFetching: boolean;
   createCard: (card: CreateCard) => Promise<Card | undefined>;
   getCard: (cardId: string) => void;
@@ -22,13 +25,17 @@ interface ICardContext {
   createItem: (checklistId: string, items: CreateItem[]) => void;
   removeChecklist: (checklistId: string) => void;
   removeCard: (cardId: string) => void;
+  listCalendarCards: (currentMonth: Date) => Promise<Card[] | void>;
 }
 
 export const CardContext = createContext<ICardContext>({} as ICardContext);
 
 export const CardProvider: React.FC = ({ children }): JSX.Element => {
   const [card, setCard] = useState<Card>({} as Card);
+  const [calendarCards, setCalendarCards] = useState<CalendarCards[]>([]);
   const [isFetching, setIsFetching] = useState(false);
+
+  const { board } = useBoard();
 
   async function refetchCard(cardId: string) {
     const response = await listOneCard(cardId);
@@ -48,6 +55,40 @@ export const CardProvider: React.FC = ({ children }): JSX.Element => {
       console.error(response.error);
     }
   }
+
+  const listCalendarCards = useCallback(
+    async (currentMonth: Date) => {
+      const response = await getCards(board._id, currentMonth);
+
+      if (response.cards) {
+        const cCards = response.cards.map((card) => {
+          if (card.deadline) {
+            return {
+              _id: card._id,
+              title: card.name,
+              resource: card.colorCode,
+              start: new Date(card.deadline),
+              end: new Date(card.deadline),
+            };
+          } else {
+            return {
+              _id: card._id,
+              title: card.name,
+              resource: card.colorCode,
+            };
+          }
+        });
+        if (cCards) {
+          setCalendarCards(cCards);
+        }
+      }
+
+      if (response.error) {
+        console.error(response.error);
+      }
+    },
+    [board._id],
+  );
 
   async function getCard(cardId: string) {
     setIsFetching(true);
@@ -142,6 +183,8 @@ export const CardProvider: React.FC = ({ children }): JSX.Element => {
         createItem,
         removeChecklist,
         removeCard,
+        listCalendarCards,
+        calendarCards,
       }}
     >
       {children}
